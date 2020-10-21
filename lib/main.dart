2eb5bd785002345c988d7bd78ba9c8e56f57756b
Muskan4010 <jainmuskan565@gmail.com>
2020-10-21 17:06:34 +0530

    more styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import './transaction.dart';
 
@@ -34,6 +36,10 @@ class _MyHomePageState extends State<MyHomePage> {
     Transaction(
         id: 't2', title: 'Mobile Phone', amount: 10350.50, date: DateTime.now())
   ];
+  void check() {
+    print(DateTime.now());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,27 +50,92 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Column(
         children: [
           Expanded(
-              child: Opacity(
-            opacity: 0.3,
-            child: Container(
-                width: double.infinity,
-                child: Card(
-                  color: Colors.red,
-                  child: Text('EXPENSES'),
-                )),
-          )),
-          Expanded(
               flex: 3,
               child: Opacity(
+                opacity: 0.3,
+                child: Container(
+                    width: double.infinity,
+                    child: Card(
+                      color: Colors.red,
+                      child: Text('EXPENSES'),
+                    )),
+              )),
+          Stack(
+            children: [
+              Opacity(
                 opacity: 0.5,
                 child: Container(
+                  height: 400.0,
                   width: double.infinity,
                   child: Card(
                     color: Colors.red,
                     child: Text('LIST OF TX'),
                   ),
                 ),
-              )),
+              ),
+              Column(children: [
+                Container(
+                  width: double.infinity,
+                  height: 80,
+                  child: Card(
+                    elevation: 20,
+                    color: Colors.redAccent[300],
+                    child: Text(
+                      'LIST OF TX',
+                      style: TextStyle(
+                        fontSize: 50,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ]),
+              Column(
+                children: transaction
+                    .map((tx) => (Card(
+                          child: Row(children: [
+                            Container(
+                              margin: EdgeInsets.symmetric(
+                                  vertical: 10, horizontal: 15),
+                              decoration: BoxDecoration(
+                                  color: Colors.purple, border: Border.all()),
+                              padding: EdgeInsets.all(8),
+                              child: Text(
+                                tx.amount.toString(),
+                                style: TextStyle(
+                                  fontSize: 11,
+                                  fontWeight: FontWeight.w200,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.all(10),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    tx.title,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 8,
+                                      color: Colors.red[400],
+                                    ),
+                                  ),
+                                  Text(
+                                    tx.date.toString(),
+                                    style: TextStyle(
+                                      color: Colors.red[400],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ]),
+                        )))
+                    .toList(),
+              )
+            ],
+          ),
         ],
       ),
     );
